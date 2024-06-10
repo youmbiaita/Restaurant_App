ad5f25dad755cefb4aeaa32477a290afb9f54a57
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-10 16:27:12 -0400

    my first commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('../config/db-connection');
+
+//User schema
+const userSchema = new Schema({
+  name: { 
+    type: String,
+    required: true, 
+    index: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true }
+});
+
+module.exports = model('user', userSchema);
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const usersCtrl = require('../controllers/usersController.js');
+
+const router = Router();
+
+//Here we call all user functions
+
+router.get('/', usersCtrl.getAllUsers);
+router.post('/', usersCtrl.createUser);
+router.get('/:id', usersCtrl.getUserById);
+router.patch('/:id', usersCtrl.updateUserById);
+router.delete('/:id', usersCtrl.deleteUserById);
+
+module.exports = router;
